cc81a6757214c45cee9208f84d9a437548f90abf
adamelkholyy <adamelkholy40@yahoo.co.uk>
2024-12-20 09:57:45 +0000

    Add human stories

--- main.py
+++ main.py
@@ -14,24 +14,29 @@ nltk.download('punkt_tab')
 
 
 # TODO
-# STOPWORDS ,#] ARE NOT BEING REMOVED
-# process human stories
-# investigate cases of pages = []
+# investigate cases of tandem pages = []
+# return 4 Nones on upack stories?
 
+# lemmatise and stem words for distribution?
 # look at bad stories
 # look at asking the LLM to **improve** the story
 # provide rating scales for the AI (/7, /5 /10 etc.)
 # tweak rating criteria
 
 ''' 
-Unpacks an individual tandom story and returns a tuple: (story prompt, title, story, reading level).
-Conditional statements are the best method for handling this data due to differing formats within the corpus. 
+Unpacks an individual story and returns a tuple: (story prompt, title, story, reading level).
 Returns None if no story is found, as there is therefore nothing to evauluate.
 '''
-def unpack_tandem_story(path: os.PathLike):
+def unpack_story(path: os.PathLike):
     with open(path) as file:
         data = json.load(file)
 
+    # unpack human story (no prompt, title, or reading level available)
+    if 'pages' in data and data['pages']:
+        story = ' '.join(line['text'] for line in data['pages'])
+        return None, None, story, None
+
+    # unpack llm story
     # separate out the story prompt if it exists
     # 'generated_prompts' format
     if 'generated_prompts' in data and data['generated_prompts']:
@@ -75,27 +80,11 @@ def unpack_tandem_story(path: os.PathLike):
     return story_prompt, title, story, reading_level
 
 
-''' generate the user prompt, i.e. format the story rating task '''
-def generate_user_prompt(story_data: tuple):
-    story_prompt, title, story, reading_age = story_data
-    user_prompt = f'''
-    Please rate the following story, which has a suggested reading age of {reading_age}:
 
-    Title: {title}
-
-    Prompt: {story_prompt}
-
-    Story: {story}
-    '''
-    return user_prompt
-
-
-
-# TODO: Progress bar
-def tandem_corpus_statistics(dir: os.PathLike):
-    print(f'Processing all tandem stories in {dir}')
+''' calculates summary statistics for all stories in the given corpus directory'''
+def calculate_corpus_statistics(dir: os.PathLike):
+    print(f'Processing all stories in {dir}')
     start = time.time()
-
     total_sentences = 0
     total_words = 0
     total_stories = 0
@@ -110,11 +99,12 @@ def tandem_corpus_statistics(dir: os.PathLike):
         if file[:3] == 'dev':
             continue
 
-        unpacked_story = unpack_tandem_story(os.path.join(dir, file))
+        unpacked_story = unpack_story(os.path.join(dir, file))
 
         # only perform operations on not None stories
         if unpacked_story:
             story_prompt, title, story, reading_age = unpacked_story
+            # skip null stories
             if not story:
                 continue
             total_stories += 1
@@ -122,7 +112,9 @@ def tandem_corpus_statistics(dir: os.PathLike):
             # word and sentence tokenizer
             words = word_tokenize(story)
             sentences = sent_tokenize(story)
-            tokenized_words = [word for word in words if word not in stopwords.words('english')]
+
+            # tokenized words with stopwords and punctuation removed
+            tokenized_words = [word.lower() for word in words if word.isalpha() and word.lower() not in stopwords.words("english")]
 
             # corpus statistic counters
             total_words += len(words)
@@ -133,28 +125,43 @@ def tandem_corpus_statistics(dir: os.PathLike):
             # ratio of unique:non-unique words
             unique_word_ratio += len(set(tokenized_words)) / len(tokenized_words)
 
-        if total_stories == 60:
-            break
-
-    print(f"Average number of words: {total_words / total_stories}")
-    print(f"Average number of sentences: {total_sentences / total_stories}")
-    print(f"Average word length: {sum(total_word_lengths) / len(total_word_lengths)}")
+    print(f"Average number of words: {round(total_words / total_stories, 2)}")
+    print(f"Average number of sentences: {round(total_sentences / total_stories, 2)}")
+    print(f"Average word length: {round(sum(total_word_lengths) / len(total_word_lengths), 2)}")
     print(f"Total words: {len(all_words)}")
     print(f"Total unique words: {len(set(all_words))}")
-    print(f"Average ratio of unique:non-unique words per story: {unique_word_ratio / total_stories}")
-    print(f'Completed in {round(time.time(), 2) - start} seconds')
-    plot_word_distribution(all_words)
+    print(f"Average ratio of unique:non-unique words per story: {round(unique_word_ratio / total_stories, 2)}")
+    print(f'Completed in {round(time.time() - start, 2)} seconds')
+    plot_word_distribution(all_words, dir)
 
 
-def plot_word_distribution(words, n=50):
+''' 
+Plots zipf's law word distribution for top n words. 
+Takes input words = [(word, count), ...] e.g. [("like", 10), ("hello", 9), ...]
+'''
+def plot_word_distribution(words, title, n=50):
     word_distribution = Counter(words).most_common(n)
     plt.bar(range(n), [w for f, w in word_distribution], align='center')
     plt.xticks(range(n), [f for f, w in word_distribution], rotation=90)
     plt.ylabel("Number of occurences")
     plt.xlabel("Word")
-    plt.title(f"Top {n} most frequently occuring words in corpus")
+    plt.title(f"Top {n} most frequently occuring words in {title} corpus")
     plt.show()
 
+
+''' generate the user prompt, i.e. format the story rating task '''
+def generate_user_prompt(story_data: tuple):
+    story_prompt, title, story, reading_age = story_data
+    user_prompt = f'''
+    Please rate the following story, which has a suggested reading age of {reading_age}:
+
+    Title: {title}
+
+    Prompt: {story_prompt}
+
+    Story: {story}
+    '''
+    return user_prompt
 
 
 ''' make a call to the gpt api '''
@@ -171,12 +178,12 @@ def make_gpt_api_call(system_prompt: str, user_prompt: str, model='gpt-4o'):
 
 
 if __name__ == '__main__':
-    tandem_corpus_statistics('data/Tandem_Data')
+    calculate_corpus_statistics('data/human books')
 
     ivo_path = 'data\\Tandem_Data\\0a9c05f0-d630-403b-a257-7a7e67452c24.json'
     astronaut_path = 'data\\Tandem_Data\\0a774cd2-ee78-4861-a33e-1a9c2f3cfed4 (1).json'
 
-    astronaut_story = unpack_tandem_story(astronaut_path)
+    astronaut_story = unpack_story(astronaut_path)
     user_prompt = generate_user_prompt(astronaut_story)
     # print(user_prompt)
 
